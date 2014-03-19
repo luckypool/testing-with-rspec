8c89086068adfe4b7b9df68df389306b35903150
luckypool <luckypool314@gmail.com>
2014-03-19 17:48:04 +0900

    Add tests むだに

--- spec/lib/dog_spec.rb
+++ spec/lib/dog_spec.rb
@@ -6,11 +6,13 @@ describe Dog do
     dog = Dog.new
     expect(dog.name).to eq 'Pochi'
   end
+  its(:name) { should eq 'Pochi' }
 
   it "has fangs" do
     dog = Dog.new
     expect(dog.fangs).to eq 2
   end
+  its(:fangs) { should be 2 }
 
   it "is alived" do
     dog = Dog.new
